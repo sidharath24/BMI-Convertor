e26bfe77075ae0eaefbe94c2537b189740b7d9cd
visualstudio <visualstudio@122-PC32>
2019-02-07 11:25:03 -0500

    new

--- BMI Calc/MainActivity.cs
+++ BMI Calc/MainActivity.cs
@@ -12,17 +12,66 @@ namespace BMI_Calc
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        TextView tHeight, tWeight, tResult1, tResult2;
+        EditText eHeight, eWeight, eResult1, eResult2;
+        Button btConv, btClr;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            tHeight = (TextView)FindViewById(Resource.Id.tvHeight);
+            tWeight = (TextView)FindViewById(Resource.Id.tvWeight);
+            tResult1 = (TextView)FindViewById(Resource.Id.tvResult1);
+            tResult2= (TextView)FindViewById(Resource.Id.tvResult2);
+            eHeight = (EditText)FindViewById(Resource.Id.etHeight);
+            eWeight = (EditText)FindViewById(Resource.Id.etWeight);
+            eResult1 = (EditText)FindViewById(Resource.Id.etResult1);
+            eResult2 = (EditText)FindViewById(Resource.Id.etResult2);
+            btConv = (Button)FindViewById(Resource.Id.btnConv);
+            btClr = (Button)FindViewById(Resource.Id.btnClr);
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            btClr.Click += delegate
+            {
+                eHeight.Text = "";
+                eWeight.Text = "";
+                eResult1.Text = "";
+                eResult2.Text = "";
+            };
+
+            btConv.Click += delegate
+            {
+                if(eHeight.Text != "" && eWeight.Text != "")
+                {
+                    double res1,eHei, eWei;
+                    eHei = double.Parse(eHeight.Text);
+                    eWei = double.Parse(eWeight.Text);
+                    res1 = Math.Round(eWei / (eHei * eHei), 1);
+                    eResult1.Text = res1.ToString();
+
+                    if(res1 <= 18.5)
+                    {
+                        eResult2.Text = "Underweight";
+                    }
+                    else if(res1 >18.5 && res1 <= 24.9)
+                    {
+                        eResult2.Text = "Normal";
+                    }
+                    else if (res1 >= 25)
+                    {
+                        eResult2.Text = "Overweight";
+                    }
+                }
+                else
+                {
+                    Toast.MakeText(this, "Please enter both the height and the weight to continue", ToastLength.Long).Show();
+                }
+
+            };
+
+
+
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
